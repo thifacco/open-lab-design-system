609093a48cbdcf7164628c529bc39dda5125dc22
Tiago Facco <thifacco@gmail.com>
2022-10-14 15:34:36 -0300

    Storybook

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-5xl text-violet-500'>Hello Vite!</h1>
+
+      <button className='bg-violet-500 font-medium px-4 rounded text-white hover:bg-violet-900'>Enviar</button>
+    </>
+  )
+}
